b9f32354b7ba7da8307068eb4696bfc794c85675
Claudio Bogossian <claudio.bogossian@funcate.org.br>
2017-12-18 11:46:39 -0200

    Fixing database creation script

--- database/migrations/2017_12_18_125246_create_nametable_table.php
+++ database/migrations/2017_12_18_125246_create_nametable_table.php
@@ -13,6 +13,20 @@ class CreateNametableTable extends Migration
      */
     public function up()
     {
+
+ Schema::dropIfExists('investiment');
+        Schema::dropIfExists('plan');
+        Schema::dropIfExists('useranalysis');
+        Schema::dropIfExists('withdraw');
+        Schema::dropIfExists('requests');
+        Schema::dropIfExists('requeststatus');
+        Schema::dropIfExists('requesttype');
+        Schema::dropIfExists('bankdata');
+        Schema::dropIfExists('balance');
+        Schema::dropIfExists('users');
+
+
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('firstname');
@@ -21,21 +35,24 @@ class CreateNametableTable extends Migration
             $table->string('document');
             $table->date('birthdate');
             $table->date('registrydate');
+	    
         });
 	Schema::create('balance', function (Blueprint $table) {
             $table->integer('userid')->unsigned();
             $table->date('date');
             $table->decimal('value');
 	    $table->foreign('userid')->references('id')->on('users');
+	    $table->primary('userid');
         });
 	Schema::create('bankdata', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('userid');
+            $table->integer('userid')->unsigned();
             $table->integer('bankid');
             $table->string('agency');
             $table->string('account');
             $table->boolean('enabled');
 	    $table->foreign('userid')->references('id')->on('users');
+
         });
 	Schema::create('requesttype', function (Blueprint $table) {
             $table->increments('id');
@@ -45,53 +62,59 @@ class CreateNametableTable extends Migration
 	Schema::create('requeststatus', function (Blueprint $table) {
             $table->increments('id');
             $table->date('name');
+
            
         });
 	Schema::create('requests', function (Blueprint $table) {
             $table->increments('id');
             $table->date('date');
-            $table->integer('requesttypeid');
-            $table->integer('requeststatusid');
+            $table->integer('requesttypeid')->unsigned();
+            $table->integer('requeststatusid')->unsigned();
             $table->boolean('approved');
 	    $table->foreign('requesttypeid')->references('id')->on('requesttype');
 	    $table->foreign('requeststatusid')->references('id')->on('requeststatus');
+
            
         });
 	Schema::create('withdraw', function (Blueprint $table) {
             $table->increments('id');
             $table->date('date');
             $table->date('enddate');
-            $table->integer('requestid');
-            $table->integer('userid');
+            $table->integer('requestid')->unsigned();
+            $table->integer('userid')->unsigned();
             $table->decimal('value');
 	    $table->foreign('userid')->references('id')->on('users');
 	    $table->foreign('requestid')->references('id')->on('requests');
+
         });
 	Schema::create('useranalysis', function (Blueprint $table) {
-            $table->integer('userid');
+            $table->integer('userid')->unsigned();
             $table->boolean('enabled');
             $table->integer('investimentpercent');
-            $table->integer('requestid');
+            $table->integer('requestid')->unsigned();
 	    $table->foreign('userid')->references('id')->on('users');
 	    $table->foreign('requestid')->references('id')->on('requests');
+	    $table->primary('userid');
         });
 	Schema::create('plan', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->integer('days');
+
         });
 	Schema::create('investiment', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('userid');
+            $table->integer('userid')->unsigned();
             $table->decimal('value');
             $table->date('initdate');
             $table->date('enddate');
-            $table->integer('planid');
+            $table->integer('planid')->unsigned();
             $table->boolean('enabled');
-            $table->integer('requestid');
+            $table->integer('requestid')->unsigned();
 	    $table->foreign('userid')->references('id')->on('users');
 	    $table->foreign('requestid')->references('id')->on('requests');
 	    $table->foreign('planid')->references('id')->on('plan');
+
         });
 
 
